ce3e69c2665e9e73123f6c5bb8e6e76221f44d9c
ColaIan <colaian.yt@gmail.com>
2025-11-10 10:52:53 +0000

    Fix web pubsub loading

--- src/lib/azure/web-pubsub.ts
+++ src/lib/azure/web-pubsub.ts
@@ -1,10 +1,13 @@
 import { env } from '$env/dynamic/private';
 import { WebPubSubServiceClient, type JSONTypes } from '@azure/web-pubsub';
 
-export const trainingService = new WebPubSubServiceClient(env.AZURE_WEB_PUBSUB_URL, 'trainings');
+let trainingService: WebPubSubServiceClient | undefined = undefined;
+export const getTrainingService = () =>
+	trainingService ||
+	(trainingService = new WebPubSubServiceClient(env.AZURE_WEB_PUBSUB_URL, 'trainings'));
 
 export const sendTrainingMessage = async (trainingId: string, data: JSONTypes) =>
-	await trainingService.group(trainingId).sendToAll(data);
+	await getTrainingService().group(trainingId).sendToAll(data);
 
 export const getTrainingGroupUrl = async (trainingId: string) =>
-	(await trainingService.getClientAccessToken({ groups: [trainingId] })).url;
+	(await getTrainingService().getClientAccessToken({ groups: [trainingId] })).url;
