27815b9e98495249a83eeb56526e41431043bd8e
ColaIan <colaian.yt@gmail.com>
2025-11-18 15:55:33 +0000

    Try fix auth client basepath

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -3,5 +3,6 @@ import { authClientPlugin } from '$lib/auth-plugin-client';
 import { inferAdditionalFields } from 'better-auth/client/plugins';
 import { createAuthClient } from 'better-auth/svelte';
 export const { signIn, signUp, signOut, useSession } = createAuthClient({
+	basePath: '/auth',
 	plugins: [inferAdditionalFields<typeof auth>(), authClientPlugin()]
 });
